f1849bc7cf5b6c92c2c41af11167c808ad408b2c
Edwin Padron <4nkaci6x@gmail.com>
2023-07-31 21:47:13 -0500

    Added db

--- index-ws.js
+++ index-ws.js
@@ -12,6 +12,12 @@ server.listen(3000, function () {
   console.log('server started on port 3000');
 });
 
+process.on('SIGINT', () => {
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 /** Begin websocket */
 const WebSocketServer = require('ws').Server;
 
@@ -27,6 +33,10 @@ wss.on('connection', function connection(ws) {
     ws.send('Welcome to my server');
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+  `);
+
   ws.on('close', function close() {
     console.log('A client has disconnected');
   });
@@ -37,3 +47,29 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+/** end websocket */
+/** begin database */
+const sqlite = require('sqlite3');
+const db = new sqlite.Database(':memory');
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE visitors (
+      count INTEGER,
+      time TEXT
+    )
+  `);
+});
+
+function getCounts() {
+  db.each('SELECT * FROM visitors', (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log('Shutting down db');
+  db.close();
+}
